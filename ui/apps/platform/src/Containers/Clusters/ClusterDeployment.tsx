33791782ac9934ab362da530abf80056013e94b5
Saif Chaudhry <schaudhr@redhat.com>
2026-03-23 11:52:50 -0700

    feat(clusters): add doc links for operator install and deprecation notices
    Add non-OpenShift documentation links to the operator installation
    instructions page and link deprecation alerts to operator install docs
    so users can easily find migration guidance.
    
    Signed-off-by: Saif Chaudhry <schaudhr@redhat.com>

--- ui/apps/platform/src/Containers/Clusters/ClusterDeployment.tsx
+++ ui/apps/platform/src/Containers/Clusters/ClusterDeployment.tsx
@@ -4,8 +4,11 @@ import { DownloadIcon } from '@patternfly/react-icons';
 import { CheckCircle } from 'react-feather';
 import { ClipLoader } from 'react-spinners';
 
+import ExternalLink from 'Components/PatternFly/IconText/ExternalLink';
 import type { ClusterManagerType } from 'types/cluster.proto';
 import useAnalytics, { LEGACY_CLUSTER_DOWNLOAD_YAML } from 'hooks/useAnalytics';
+import useMetadata from 'hooks/useMetadata';
+import { getVersionedDocs } from 'utils/versioning';
 
 export type ClusterDeploymentProps = {
     clusterCheckedIn: boolean;
@@ -27,6 +30,7 @@ function ClusterDeployment({
     managerType,
 }: ClusterDeploymentProps): ReactElement {
     const { analyticsTrack } = useAnalytics();
+    const { version } = useMetadata();
 
     let managerTypeTitle = 'Dynamic configurations are automatically applied';
     let managerTypeText =
@@ -62,6 +66,25 @@ function ClusterDeployment({
                         </Content>
                         <Content component="p">
                             Use the Kubernetes operator to install secured cluster services instead.
+                            {version && (
+                                <>
+                                    {' '}
+                                    See{' '}
+                                    <ExternalLink>
+                                        <a
+                                            href={getVersionedDocs(
+                                                version,
+                                                'installing/installing-rhacs-on-red-hat-openshift#install-secured-cluster-ocp'
+                                            )}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                        >
+                                            operator installation documentation
+                                        </a>
+                                    </ExternalLink>
+                                    .
+                                </>
+                            )}
                         </Content>
                     </Alert>
                     <Title headingLevel="h2">Download manifest bundle</Title>
